8e19398a99e9d2c086239ac9c06109f874d35099
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-03-24 02:16:07 +0200

    The setup

---
+++ frontend/notes-app/src/pages/Login/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/navbar'
+
+const Login = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Login
