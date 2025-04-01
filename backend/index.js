79b158be8752c3174cb11d46143a4deb04a42d92
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-04-01 21:13:15 +0200

    Add New Note API + Edit Note API

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,17 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+
+
 const User = require('./models/user.model');
+const Note = require('./models/note.model');
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 const jwt = require('jsonwebtoken');
-const { authenticationToken } = require('./utilities');
+const { authenticateToken } = require('./utilities');
 
 app.use(express.json())
 app.use(cors({
@@ -98,7 +101,78 @@ app.post('/login', async (req, res) => {
     } else {
         return res.status(400).json({ error: true, message: 'Invalid Credentials', });
     }
-})
+});
+
+// Add Note
+app.post('/add-note', authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body;
+    const { user } = req.user;
+
+    if (!title) {
+        return res.status(400).json({ error: true, message: 'Title is required' });
+    }
+
+    if (!content) {
+        return res.status(400).json({ error: true, message: 'Content is required' });
+    }
+
+    try {
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: user._id,
+        });
+
+        await note.save();
+        return res.json({
+        error: false,
+        note,
+        message: 'Note added successfully',
+    });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: 'Internal server error' 
+            });
+    }
+});
+
+// Edit Note
+app.post('/edit-note/:noteId', authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if ( !title && !content && !tags ) {
+        return res.status(400).json({ error: true, message: "No changes provided" });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+        if (!note) {
+            return res.status(404).json({ error: true, message: 'Note not found' });
+        }
+
+        if (title) note.title = title;
+        if (content) note.content = content;
+        if (tags) tags.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({ 
+            error: false,
+            note,
+            message: "Note updated successfully",
+         })
+
+    } catch (erorr) {
+        return res.status(500).json({
+            error: true,
+            message: 'Internal server error' 
+        });
+    }
+});
 
 app.listen(8000);
 
