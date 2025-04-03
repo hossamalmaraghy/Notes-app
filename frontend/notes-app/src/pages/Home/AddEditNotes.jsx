d1364df069b24d1826da115e68a83735690a58e0
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-04-03 06:41:26 +0200

    All Notes API + Add New Note API  + Edit Note API Integrations

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,21 +1,64 @@
 import React, { useState } from 'react'
 import TagInput from '../../components/Input/TagInput'
 import { MdClose } from 'react-icons/md'
+import axiosInstance from '../../utils/axiosInstance';
 
-const AddEditNotes = ({ noteData, type, onClose }) => {
+const AddEditNotes = ({ noteData, type, getAllNotes, onClose}) => {
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [tags, setTags] = useState([]);
+    const [title, setTitle] = useState(noteData?.title || '');
+    const [content, setContent] = useState(noteData?.content || '');
+    const [tags, setTags] = useState(noteData?.tags || []);
 
     const [error, setError] = useState(null);
 
+    // Aadd Note
     const addNewNote = async () => {
-        
+        try {
+            const response = await axiosInstance.post('/add-note', {
+                title,
+                content,
+                tags,
+            });
+
+            if (response.data && response.data.note) {
+                getAllNotes();
+                onClose();
+            }
+        } catch (error) {
+            if (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) {
+                setError(error.response.data.message);
+            }
+        }
     }
 
+    // Edit Note
     const editNote = async () => {
+        const noteId = noteData._id;
 
+        try {
+            const response = await axiosInstance.put('/edit-note/' + noteId, {
+                title,
+                content,
+                tags,
+            });
+
+            if (response.data && response.data.note) {
+                getAllNotes();
+                onClose();
+            }
+        } catch (error) {
+            if (
+                error.response &&
+                error.response.data &&
+                error.response.data.message
+            ) {
+                setError(error.response.data.message);
+            }
+        }
     }
 
     const handleAddNote = () => {
@@ -77,7 +120,7 @@ const AddEditNotes = ({ noteData, type, onClose }) => {
     <button className="btn-primary font-medium mt-5 p-3"
          onClick={handleAddNote}
          >
-         ADD
+         {type === 'edit' ? 'UPDATE' : 'ADD'}
         </button>
     </div>
   )
--- frontend/notes-app/src/pages/Home/home.jsx
+++ frontend/notes-app/src/pages/Home/home.jsx
@@ -1,103 +1,126 @@
-import React, { useState } from 'react'
-import Navbar from '../../components/Navbar/navbar'
-import NoteCard from '../../components/Cards/NoteCard'
-import { MdAdd } from 'react-icons/md'
-import AddEditNotes from './AddEditNotes'
-import Modal from 'react-modal'
-import { useNavigate } from 'react-router-dom'
-import axiosInstance from '../../utils/axiosInstance'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { useEffect } from "react";
 
 const Home = () => {
-
   const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
-    type: 'add',
+    type: "add",
     data: null,
   });
+
+  const [allNotes, setAllNotes] = useState([]);
 
   const [userInfo, setUserInfo] = useState(null);
 
   const navigate = useNavigate();
 
+  const handleEdit = (noteDetails) => {
+    setOpenAddEditModal({ isShown: true, data: noteDetails, type: 'edit' });
+  };
+
   // Get User Info
   const getUserInfo = async () => {
     try {
-      const response = await axiosInstance.get('/get-user');
+      const response = await axiosInstance.get("/get-user");
       if (response.data && response.data.user) {
         setUserInfo(response.data.user);
       }
     } catch (error) {
       if (error.response.status === 401) {
         localStorage.clear();
-        navigate('/login');
+        navigate("/login");
       }
     }
   };
 
+  // Get All Notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
   useEffect(() => {
+    getAllNotes();
     getUserInfo();
     return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
-    <Navbar userInfo={userInfo}/>
+      <Navbar userInfo={userInfo} />
 
-    <div className="container mx-auto">
-
-      <div className='grid grid-cols-3 gap-4 mt-8'>
-      <NoteCard 
-        title='Meeting on 7th April'
-        date='3rd Apr 2025'
-        content='Meeting on 7th April 2025'
-        tags='#Meeting'
-        isPinned={true}
-        onEdit={() => {}}
-        onDelete={() => {}}
-        onPinNote={() => {}}
-      />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          {allNotes.map((item, index) => (
+            <NoteCard
+              key={item._id}
+              title={item.title}
+              date={item.createdOn}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => {handleEdit(item)}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+          ))}
+        </div>
       </div>
-      
-    </div>
 
-    <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" 
-      onClick={() => {
-        setOpenAddEditModal({
-          isShown: true,
-          type: 'add',
-          data: null
-        })
-      }}>
-      <MdAdd className='text-[32px] text-white' />
-    </button>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {
+          setOpenAddEditModal({
+            isShown: true,
+            type: "add",
+            data: null,
+          });
+        }}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
 
-    <Modal 
-      isOpen={openAddEditModal.isShown}
-      onRequestClose={() => {}}
-      style={{
-        overlay: {
-          backgroundColor: 'rgba(0,0,0,0.2',
-        }
-      }}
-      contentLabel=''
-      className='w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll'
-      >
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll">
+        <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({
+              isShown: false,
+              type: "add",
+              data: null,
+            });
+          }}
 
-    <AddEditNotes 
-      type={openAddEditModal.type}
-      noteData={openAddEditModal.data}
-      onClose={() => {
-        setOpenAddEditModal({
-          isShown: false,
-          type: 'add',
-          data: null
-        })
-      }}
-    />
-    </Modal>
+          getAllNotes={getAllNotes}
+
+        />
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
